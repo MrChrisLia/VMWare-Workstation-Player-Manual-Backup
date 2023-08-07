382e7b4328361bc7aae628b3ca803c2888e6d105
Christopher L <60084418+MrChrisLia@users.noreply.github.com>
2023-08-07 19:37:58 +0800

    Update Backup_VMware.py
    Made it create a new folder based on the date

--- Backup_VMware.py
+++ Backup_VMware.py
@@ -1,17 +1,24 @@
 from plyer import notification
 import shutil
 import ctypes
+import datetime
 
 
 # Backups of the directory of the Kali installation and copies it to a directory called Kali_Backup
 # If successful, will create a Windows notification
 
+# Get the current date
+current_date = datetime.date.today()
+
+# Format and print the date in "YYYY_MM_DD" format
+formatted_date = current_date.strftime('%Y_%m_%d')
+
 def backup():
     success_msg = 'Your automated backup has successfully completed.'
 
-    # r'C:\path\to\directory' lets us avoid having to escape with backslashes 
-    source_directory = r'C:\path\to\directory'
-    destination_directory = r'C:\path\to\directory'
+    # r'C:/path/to/directory' lets us avoid having to escape with backslashes 
+    source_directory = r'C:\Users\chris\Documents\Virtual Machines\kali-linux-2023.2-vmware-amd64.vmwarevm'
+    destination_directory = r'C:\Users\chris\Documents\Virtual Machine Backups\Kali_Backup' + '_' + str(formatted_date)
 
     shutil.copytree(source_directory, destination_directory)
 
@@ -46,7 +53,7 @@ def run_script_prompt():
 
 
 if __name__ == '__main__':
-    print("Starting Backup...")
+    print("[+] The backup will start after accepting the prompt...")
     run_script_prompt()
-    print("Backup complete... this window will automatically close...")
+    print("{+] Backup complete... this window will automatically close in a few seconds... \n[+] Check the notifications for more details.")
     exit()
